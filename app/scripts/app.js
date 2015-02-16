75fd4ab2ef9ed70d940d3c3a0e02d11f95b9b5a0
Paul D'Ambra <pdambra@roishoppermarketing.com>
2015-02-16 23:13:10 +0000

    can sort data in table

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,6 @@
 'use strict';
 
 var React = window.React = require('react');
-var Timer = require('./ui/Timer');
 var mountTodoNode = document.getElementById('app');
 var mountTableNode = document.getElementById('table-container');
 var PayTable = require('./payTable');
--- app/scripts/payTable.js
+++ app/scripts/payTable.js
@@ -16,20 +16,59 @@ var PayRow = React.createClass({
     }
 });
 
+function sortAscending(a,b) {
+  if (a.year < b.year)
+     return -1;
+  if (a.year > b.year)
+    return 1;
+  return 0;
+};
+
+function sortDescending(a,b) {
+  if (a.year > b.year)
+     return -1;
+  if (a.year < b.year)
+    return 1;
+  return 0;
+};
+
 var PayTable = React.createClass({
+    getInitialState: function() {
+        return {
+            sortDirection: 'descending',
+            data: this.props.payYears.sort(sortDescending)
+        };
+    },
+    sortData: function() {
+        if(this.state.sortDirection==='descending') {
+            this.setState({ 
+                sortDirection: 'ascending',
+                data: this.props.payYears.sort(sortAscending)
+            });
+        } else {
+            this.setState({ 
+                sortDirection: 'descending',
+                data: this.props.payYears.sort(sortDescending)
+            });
+        }
+    },
     render: function() {
         return (
             <table className="table table-striped">
                 <thead>
                     <tr>
-                        <th>Year</th>
+                        <th onClick={this.sortData}
+                            className={this.state.sortDirection}
+                        >
+                            Year
+                        </th>
                         <th>All</th>
                         <th>Full-time</th>
                         <th>Part-time</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {this.props.payYears.map(function(payYear) {
+                    {this.state.data.map(function(payYear) {
                       return <PayRow key={payYear.year} payYear={payYear} />;
                     })}
                 </tbody>
