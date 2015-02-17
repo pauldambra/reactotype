509fc527966f1629cacf694ffba0f7cf937a1a0f
Paul D'Ambra <pdambra@roishoppermarketing.com>
2015-02-17 16:19:29 +0000

    add postal without using it and log changes to filter box values without using them

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,12 +2,16 @@
 'use strict';
 
 var React = window.React = require('react');
-var mountTodoNode = document.getElementById('app');
-var mountTableNode = document.getElementById('table-container');
 var PayTable = require('./payTable');
 var data = require('./ui/payData');
 var TodoApp = require('./todo');
+var FilterBox = require('./filterBox');
+
+var mountTodoNode = document.getElementById('app');
+var mountTableNode = document.getElementById('table-container');
+var mountFilterNode = document.getElementById('filter-container');
 
 React.render(<PayTable payYears={data} />, mountTableNode);
 React.render(<TodoApp />, mountTodoNode);
+React.render(<FilterBox initialEarliest={1977} initialLatest={2010}/>, mountFilterNode);
 
---
+++ app/scripts/filterBox.js
@@ -0,0 +1,52 @@
+/** @jsx React.DOM */
+'use strict';
+
+var React = window.React = require('react');
+var postal = window.postal = require('postal');
+
+var FilterBox = React.createClass({
+    getInitialState: function() {
+        return {
+            earliest: this.props.initialEarliest,
+            latest: this.props.initialLatest
+        };
+    },
+    handleEarliestChange: function(event) {
+    	this.setState({earliest: parseInt(event.target.value, 10)}, function() {
+    		console.log(this.state);
+    	});
+    },
+    handleLatestChange: function(event) {
+    	this.setState({latest: parseInt(event.target.value, 10)}, function() {
+    		console.log(this.state);
+    	});
+    },
+    render: function() {
+        return (
+        	<div className="col-xs-12">
+        		<div className="form-group">
+        			<label htmlFor="earliest">Earliest</label>
+        			<input type="number" 
+        				   name="earliest"
+        				   className="form-control"
+        				   defaultValue={this.state.earliest}
+        				   min={this.state.earliest} 
+        				   max={this.state.latest}
+        				   onChange={this.handleEarliestChange}/>
+        		</div>
+        		<div className="form-group">
+        			<label htmlFor="latest">Latest</label>
+        			<input type="number" 
+        				   name="latest" 
+        				   className="form-control"
+        				   defaultValue={this.state.latest}
+        				   min={this.state.earliest} 
+        				   max={this.state.latest}
+        				   onChange={this.handleLatestChange}/>
+        		</div>
+        	</div>
+        );
+    }
+});
+
+module.exports = FilterBox;
