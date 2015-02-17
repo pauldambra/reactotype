480090f8d8c2b59304620a3e2a5d36c32c93abf2
Paul D'Ambra <pdambra@roishoppermarketing.com>
2015-02-17 21:41:19 +0000

    remove todo app from yeoman scaffolding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,16 +2,14 @@
 'use strict';
 
 var React = window.React = require('react');
+
 var PayTable = require('./payTable');
-var data = require('./ui/payData');
-var TodoApp = require('./todo');
+var data = require('./payData');
 var FilterBox = require('./filterBox');
 
-var mountTodoNode = document.getElementById('app');
 var mountTableNode = document.getElementById('table-container');
 var mountFilterNode = document.getElementById('filter-container');
 
 React.render(<PayTable payYears={data} />, mountTableNode);
-React.render(<TodoApp />, mountTodoNode);
 React.render(<FilterBox initialEarliest={1997} initialLatest={2014}/>, mountFilterNode);
 
