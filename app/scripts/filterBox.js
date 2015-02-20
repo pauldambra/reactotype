12c4a96b1f3d418e312a1dc060320ec454179723
Paul D'Ambra <paul.dambra@gmail.com>
2015-02-20 11:56:50 +0000

    componentise the year input boxes

--- app/scripts/filterBox.js
+++ app/scripts/filterBox.js
@@ -5,57 +5,49 @@ var React = window.React = require('react');
 var postal = window.postal = require('postal');
 var bus = require('./messageBus');
 
+var YearFilterInput = React.createClass({
+	publishOnChange: function(event) {
+		var eventData = {};
+		eventData[this.props.name.toLowerCase()] = 
+			parseInt(event.target.value, 10);
+
+		postal.publish({
+			channel: bus.channels.filters,
+			topic : bus.topics.filters.yearBoundsChange,
+			data: eventData
+		});
+	},
+	render: function() {
+		return (
+				<div className="form-group">
+					<label htmlFor={this.props.name}>{this.props.name}</label>
+					<input type="number" 
+						   name={this.props.name}
+						   className="form-control"
+						   defaultValue={this.props.default}
+						   min={this.props.initialEarliest} 
+						   max={this.props.initialLatest}
+						   onChange={this.publishOnChange}/>
+				</div>
+		);
+	}
+});
+
 var FilterBox = React.createClass({
-    getInitialState: function() {
-        return {
-            earliest: this.props.initialEarliest,
-            latest: this.props.initialLatest
-        };
-    },
-    handleEarliestChange: function(event) {
-    	this.setState({earliest: parseInt(event.target.value, 10)}, function() {
-    		postal.publish({
-	          channel: bus.channels.filters,
-	          topic : bus.topics.filters.yearBoundsChange,
-    			data: this.state
-    		});
-    	});
-    },
-    handleLatestChange: function(event) {
-    	this.setState({latest: parseInt(event.target.value, 10)}, function() {
-    		postal.publish({
-          channel: bus.channels.filters,
-          topic : bus.topics.filters.yearBoundsChange,
-    			data: this.state
-    		});
-    	});
-    },
-    render: function() {
-        return (
-        	<div className="col-xs-12">
-        		<div className="form-group">
-        			<label htmlFor="earliest">Earliest</label>
-        			<input type="number" 
-        				   name="earliest"
-        				   className="form-control"
-        				   defaultValue={this.state.earliest}
-        				   min={this.props.initialEarliest} 
-        				   max={this.props.initialLatest}
-        				   onChange={this.handleEarliestChange}/>
-        		</div>
-        		<div className="form-group">
-        			<label htmlFor="latest">Latest</label>
-        			<input type="number" 
-        				   name="latest" 
-        				   className="form-control"
-        				   defaultValue={this.state.latest}
-        				   min={this.props.initialEarliest} 
-        				   max={this.props.initialLatest}
-        				   onChange={this.handleLatestChange}/>
-        		</div>
-        	</div>
-        );
-    }
+	render: function() {
+		return (
+			<div className="col-xs-12">
+				<YearFilterInput name="Earliest" 
+								 default={this.props.initialEarliest}
+								 initialEarliest={this.props.initialEarliest}
+								 initialLatest={this.props.initialLatest} />
+				<YearFilterInput name="Latest" 
+								 default={this.props.initialLatest}
+								 initialEarliest={this.props.initialEarliest}
+								 initialLatest={this.props.initialLatest} />
+			</div>
+		);
+	}
 });
 
 module.exports = FilterBox;
--- app/scripts/payTable.js
+++ app/scripts/payTable.js
@@ -1,104 +1,118 @@
 /** @jsx React.DOM */
 'use strict';
 
-var React = window.React = require('react');
+var React = window.React = require('react/addons');
 var postal = window.postal = require('postal');
 var bus = require('./messageBus');
 
 var PayRow = React.createClass({
-    render: function() {
-        return (
-            <tr>
-                <td>{this.props.payYear.year}</td>
-                <td>{this.props.payYear.all}</td>
-                <td>{this.props.payYear.fulltime}</td>
-                <td>{this.props.payYear.parttime}</td>
-            </tr>
-        );
-    }
+	render: function() {
+		return (
+			<tr>
+				<td>{this.props.payYear.year}</td>
+				<td>{this.props.payYear.all}</td>
+				<td>{this.props.payYear.fulltime}</td>
+				<td>{this.props.payYear.parttime}</td>
+			</tr>
+		);
+	}
 });
 
 function sortAscending(a,b) {
-  if (a.year < b.year)
-     return -1;
-  if (a.year > b.year)
-    return 1;
-  return 0;
+	if (a.year < b.year)
+	   return -1;
+	if (a.year > b.year)
+	   return 1;
+	return 0;
 };
 
 function sortDescending(a,b) {
-  if (a.year > b.year)
-     return -1;
-  if (a.year < b.year)
-    return 1;
-  return 0;
+	if (a.year > b.year)
+	   return -1;
+	if (a.year < b.year)
+	   return 1;
+	return 0;
 };
 
+function filterPayData(data, options) {
+	if (options.earliest) {
+		data = data.filter(function(element) {
+					return element.year >= options.earliest;
+				});
+	}
+	if (options.latest) {
+		data = data.filter(function(element) {
+					return element.year <= options.latest;
+				});
+	}
+	if (options.sortDirection) {
+		data = data.sort(options.sortDirection==='descending' 
+							? sortDescending
+							: sortAscending);
+	}
+	return data;
+}
+
 var PayTable = React.createClass({
-    getInitialState: function() {
-        return {
-            sortDirection: 'descending',
-            data: this.props.payYears.sort(sortDescending)
-        };
-    },
-    preparePayData: function(data, options) {
-        if (options.yearBounds) {
-            data = data.filter(function(element) {
-                        return element.year >= options.yearBounds.earliest 
-                            && element.year <= options.yearBounds.latest;
-                    })
-        }
-        if (options.sortDirection) {
-            data = data.sort(options.sortDirection==='descending' 
-                                ? sortDescending
-                                : sortAscending);
-        }
-        this.setState({data: data});
-    },
-    sortData: function() {
-        this.setState({sortDirection: this.state.sortDirection === 'descending'
-                                     ? 'ascending'
-                                     : 'descending'}, 
-                      function() {
-                        this.preparePayData(this.props.payYears, this.state);
-                      })
-    },
-    filterData: function(filterBounds) {
-        this.setState({yearBounds: filterBounds}, function() {
-            this.preparePayData(this.props.payYears, this.state);
-        });
-      },
-        componentWillMount: function() {
-        postal.subscribe({
-          channel: bus.channels.filters,
-          topic : bus.topics.filters.yearBoundsChange,
-          callback: function(d, e) {
-            this.filterData(d);
-          }
-        }).context(this);
-      },
-    render: function() {
-        return (
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th onClick={this.sortData}
-                            className={this.state.sortDirection}>
-                            Year
-                        </th>
-                        <th>All</th>
-                        <th>Full-time</th>
-                        <th>Part-time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.data.map(function(payYear) {
-                      return <PayRow key={payYear.year} payYear={payYear} />;
-                    })}
-                </tbody>
-            </table>
-        );
-    }
+	getInitialState: function() {
+		return {
+			sortDirection: 'descending',
+			data: this.props.payYears.sort(sortDescending)
+		};
+	},
+	preparePayData: function(data, options) {
+		data = filterPayData(data, options);
+		this.setState({data: data});
+	},
+	sortData: function() {
+		var newDirection = this.state.sortDirection === 'descending'
+									 ? 'ascending'
+									 : 'descending';
+		this.setState({sortDirection: newDirection}, 
+						function() {
+						  this.preparePayData(
+                                this.props.payYears, 
+                                this.state);
+						})
+	},
+	filterData: function(filterBounds) {
+		var newState = React.addons.update(this.state, {$merge: filterBounds});
+
+		this.setState(newState, function() {
+			this.preparePayData(this.props.payYears, this.state);
+		});
+	},
+	componentWillMount: function() {
+		postal.subscribe({
+			channel: bus.channels.filters,
+			topic : bus.topics.filters.yearBoundsChange,
+			callback: function(d, e) {
+			this.filterData(d);
+			}
+		}).context(this);
+	},
+	render: function() {
+		return (
+			<table className="table table-striped">
+				<thead>
+					<tr>
+						<th onClick={this.sortData}
+							className={this.state.sortDirection}>
+							Year
+						</th>
+						<th>All</th>
+						<th>Full-time</th>
+						<th>Part-time</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.state.data.map(function(payYear) {
+						return <PayRow key={payYear.year} payYear={payYear} />;
+					})}
+				</tbody>
+			</table>
+		);
+	}
 });
 
 module.exports = PayTable;
