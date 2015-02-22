da45c6c34cf271c4acc95f6ba17f826eaa9296c2
Paul D'Ambra <paul.dambra@gmail.com>
2015-02-22 13:33:44 +0000

    add a paytablespec file

---
+++ tests/payTableSpec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+
+describe('the pay table', function() {
+	it('shows the data');
+	it('sorts the data when the year header is clicked');
+	it('filters the data when the messagebus publishes');
+});
